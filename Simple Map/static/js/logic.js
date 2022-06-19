89f73fe8a9c5ea81dc22396a82154cb5bd5c041d
unknown <jbancroftsmith@gmail.com>
2022-06-19 12:36:40 -0400

    add tor airport data

--- Simple Map/static/js/logic.js
+++ Simple Map/static/js/logic.js
@@ -85,7 +85,7 @@ console.log("working");
 //let marker = L.marker([34.0522, -118.2437]).addTo(map);
 
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let light = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
@@ -100,15 +100,15 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
 
 // Create a base layer that holds both maps.
 let baseMaps = {
-    Street: streets,
+    Light: light,
     Dark: dark
   };
 
 // Create the map object with center, zoom level and default layer.
 let map = L.map('mapid', {
-    center: [30, 30],
+    center: [44.0, -80.0],
     zoom: 2,
-    layers: [streets]
+    layers: [light]
 })
 
 // Pass our map layers into our layers control and add the layers control to the map.
@@ -117,11 +117,14 @@ L.control.layers(baseMaps).addTo(map);
 // // Then we add our 'graymap' tile layer to the map.
 // streets.addTo(map);
 
-// Accessing the airport GeoJSON URL
-let airportData = "https://raw.githubusercontent.com/JSmith1826/Mapping_earthquakes/Mapping_GeoJSON_Points/Simple%20Map/static/data/majorAirports.json";
+// Accessing the Toronto airline routes GeoJSON URL.
+let torontoData = "https://raw.githubusercontent.com/JSmith1826/Mapping_earthquakes/Mapping_GeoJSON_Points/Simple%20Map/static/data/torontoRoutes.json";
+
+// // Accessing the airport GeoJSON URL
+// let airportData = "https://raw.githubusercontent.com/JSmith1826/Mapping_earthquakes/Mapping_GeoJSON_Points/Simple%20Map/static/data/majorAirports.json";
 
 // Grabbing our GeoJSON data.
-d3.json(airportData).then(function(data) {
+d3.json(torontoData).then(function(data) {
     console.log(data);
   // Creating a GeoJSON layer with the retrieved data.
   L.geoJSON(data).addTo(map);
