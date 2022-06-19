5791317e586c7ac708fdbda19782ae9b1024647c
unknown <jbancroftsmith@gmail.com>
2022-06-19 11:57:39 -0400

    made lines

--- Simple Map/static/js/logic.js
+++ Simple Map/static/js/logic.js
@@ -1,8 +1,8 @@
 // AAdd consol log to check to see if code is working
 console.log("working");
 
-// Create a new map object with options
-let map = L.map('mapid').setView([34.0522, -118.2437], 4);
+// Create the map object with center at the San Francisco airport.
+let map = L.map('mapid').setView([37.6213, -122.3790], 5);
 
 // // Add a circle to the map
 // L.circleMarker([34.0522, -118.2437], {
@@ -10,6 +10,19 @@ let map = L.map('mapid').setView([34.0522, -118.2437], 4);
 //     color: "black",
 //     fillColor: '#ffffa1'
 //  }).addTo(map);
+
+// Coordinates for each point to be used in the polyline.
+let line = [
+    [33.9416, -118.4085],
+    [37.6213, -122.3790],
+    [40.7899, -111.9791],
+    [47.4502, -122.3088]
+  ];
+
+// Create a polyline using the line coordinates and make the line red.
+L.polyline(line, {
+    color: "yellow"
+  }).addTo(map);
 
 
 // Get data from cities.js
@@ -29,7 +42,7 @@ cityData.forEach(function(city) {
 //let marker = L.marker([34.0522, -118.2437]).addTo(map);
 
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
